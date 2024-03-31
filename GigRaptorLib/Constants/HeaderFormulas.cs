6f7e6cc812d9d057cca0446bd810223b69dc24e9
Justin <khanjal@gmail.com>
2024-03-30 21:18:55 -0400

    Started work on refactoring sheet generation

--- GigRaptorLib/Constants/HeaderFormulas.cs
+++ GigRaptorLib/Constants/HeaderFormulas.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GigRaptorLib.Constants
+﻿namespace GigRaptorLib.Constants
 {
     public static class HeaderFormulas
     {
---
+++ GigRaptorLib/Constants/SheetsConfig.cs
@@ -0,0 +1,33 @@
+﻿using GigRaptorLib.Enums;
+using GigRaptorLib.Models;
+using GigRaptorLib.Utilities.Extensions;
+
+namespace GigRaptorLib.Constants
+{
+    public static class SheetsConfig
+    {
+        public static SheetModel AddressSheet => new()
+        {
+            Name = SheetEnum.ADDRESSES.DisplayName(),
+            CellColor = ColorEnum.LIGHT_CYAN,
+            TabColor = ColorEnum.CYAN,
+            FreezeColumnCount = 1,
+            FreezeRowCount = 1,
+            ProtectSheet = true,
+            Headers = [
+                new SheetCellModel { Name = HeaderEnum.ADDRESS.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.TRIPS.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.PAY.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.TIPS.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.BONUS.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.TOTAL.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.CASH.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.AMOUNT_PER_TRIP.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.DISTANCE.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.AMOUNT_PER_DISTANCE.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.VISIT_FIRST.DisplayName() },
+                new SheetCellModel { Name = HeaderEnum.VISIT_LAST.DisplayName() }
+            ]
+        };
+    }
+}
