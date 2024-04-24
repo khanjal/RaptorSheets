4779a3022cb498b17bdb03e165f5dc30e9f5a24e
Justin <khanjal@gmail.com>
2024-04-24 15:33:18 -0400

    Added additional check header tests.

--- GigRaptorLib.Tests/Utilities/HeaderHelperTests/CheckSheetHeaderTests.cs
+++ GigRaptorLib.Tests/Utilities/HeaderHelperTests/CheckSheetHeaderTests.cs
@@ -3,6 +3,7 @@ using GigRaptorLib.Enums;
 using GigRaptorLib.Mappers;
 using GigRaptorLib.Models;
 using GigRaptorLib.Tests.Data;
+using GigRaptorLib.Tests.Data.Helpers;
 using GigRaptorLib.Utilities;
 using GigRaptorLib.Utilities.Extensions;
 using Google.Apis.Sheets.v4.Data;
@@ -31,7 +32,6 @@ public class CheckSheetHeaderTests
 
     readonly GoogleDataFixture fixture;
     private static IList<MatchedValueRange>? _matchedValueRanges;
-    private static Dictionary<int, string>? _headers;
 
     public CheckSheetHeaderTests(GoogleDataFixture fixture)
     {
@@ -41,11 +41,47 @@ public class CheckSheetHeaderTests
 
     [Theory]
     [MemberData(nameof(Sheets))]
-    public void GivenFullHeadersCheck_ThenReturnNoMessages(SheetModel sheet, SheetEnum sheetEnum)
+    public void GivenFullHeaders_ThenReturnNoMessages(SheetModel sheet, SheetEnum sheetEnum)
     {
-        var values = _matchedValueRanges?.Where(x => x.DataFilters[0].A1Range == sheetEnum.DisplayName()).First().ValueRange.Values;
+        var values = _matchedValueRanges?.Where(x => x.DataFilters[0].A1Range == sheetEnum.DisplayName()).First().ValueRange.Values.ToList();
         var messages = HeaderHelper.CheckSheetHeaders(values!, sheet);
 
         messages.Should().BeEmpty();
+    }
+
+    [Theory]
+    [MemberData(nameof(Sheets))]
+    public void GivenMissingHeaders_ThenReturnErrorMessages(SheetModel sheet, SheetEnum sheetEnum)
+    {
+        var values = _matchedValueRanges?.Where(x => x.DataFilters[0].A1Range == sheetEnum.DisplayName()).First().ValueRange.Values;
+
+        var headerValues = new List<IList<object>>
+        {
+            values![0].ToList().GetRange(0, values[0].Count - 3)
+        };
+
+        var errorMessages = HeaderHelper.CheckSheetHeaders(headerValues!, sheet).Where(x => x.Type == MessageEnum.Error);
+
+        errorMessages.Should().NotBeNullOrEmpty();
+        
+    }
+
+    [Theory]
+    [MemberData(nameof(Sheets))]
+    public void GivenMisorderedHeaders_ThenReturnWarningMessages(SheetModel sheet, SheetEnum sheetEnum)
+    {
+        var values = _matchedValueRanges?.Where(x => x.DataFilters[0].A1Range == sheetEnum.DisplayName()).First().ValueRange.Values;
+
+        var headerValues = new List<IList<object>>
+        {
+            values![0].ToList().GetRange(0, values[0].Count - 1)
+        };
+
+        var headerOrder = new int[] { 0 }.Concat([.. RandomHelpers.GetRandomOrder(1, headerValues![0].Count - 1)]).ToArray();
+        var randomValues = RandomHelpers.RandomizeValues(headerValues, headerOrder);
+
+        var warningMessages = HeaderHelper.CheckSheetHeaders(randomValues!, sheet).Where(x => x.Type == MessageEnum.Warning);
+
+        warningMessages.Should().NotBeNullOrEmpty();
     }
 }
