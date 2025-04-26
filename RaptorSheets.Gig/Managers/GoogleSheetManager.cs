e7c725568df2ec3b6c8464e7bf2042179f875b90
Justin <khanjal@gmail.com>
2025-04-26 10:07:57 -0400

    Changed to handle common sheets.

--- RaptorSheets.Gig/Managers/GoogleSheetManager.cs
+++ RaptorSheets.Gig/Managers/GoogleSheetManager.cs
@@ -261,7 +261,7 @@ public class GoogleSheetManager : IGoogleSheetManager
 
         foreach (var sheetTitle in sheetTitles)
         {
-            sheetEntity.Messages.Add(MessageHelpers.CreateWarningMessage($"{sheetTitle.GetValueFromName<SheetEnum>()} created", MessageTypeEnum.CREATE_SHEET));
+            sheetEntity.Messages.Add(MessageHelpers.CreateWarningMessage($"{sheetTitle.ToUpperInvariant()} created", MessageTypeEnum.CREATE_SHEET));
         }
 
         return sheetEntity;
