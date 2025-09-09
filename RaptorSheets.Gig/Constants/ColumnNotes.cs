767298160673282cc5797a62b3640418db89dd74
Justin <khanjal@gmail.com>
2025-09-09 17:19:01 -0400

    Update ColumnNotes.cs

--- RaptorSheets.Gig/Constants/ColumnNotes.cs
+++ RaptorSheets.Gig/Constants/ColumnNotes.cs
@@ -13,7 +13,7 @@ public static class ColumnNotes
     public static string Place => "Location of pickup (delivery).";
     public static string ShiftDistance => "Distance not accounted for on the Requests/Trips sheet.";
     public static string ShiftKey => "Used to connect Shifts to Requests/Trips sheet.";
-    public static string ShiftNumber => $"Shift Number 1-9{(char)10}{(char)10}Leave blank if there is only shift for that service for that day.";
+    public static string ShiftNumber => $"Shift Number 1-9{(char)10}{(char)10}Leave blank if there is only one shift for that service for that day.";
     public static string ShiftTrips => $"Requests/Deliveries/Trips{(char)10}{(char)10}Use this column if you don't track requests or need to increase the number.";
     public static string TimeOmit => $"Omit time from non service specific totals. Mainly useful if you multi app so you can get a more accurate $/hour calculation.{(char)10}{(char)10}Active time is still counted for the day from omitted shifts.{(char)10}{(char)10}IE: Omit Uber if you have it also running during DoorDash.";
     public static string TotalDistance => "Total Miles/Kilometers from Requests and Shifts";
